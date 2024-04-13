3f948e89b301bcf5dd4b2a9f940ff7f37d8f74c6
nidharshni <110120985+HND04@users.noreply.github.com>
2024-04-13 15:21:11 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -8,7 +8,7 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
+            <h1>Hello mf's</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
